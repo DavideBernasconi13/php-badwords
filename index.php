10d40ce4f6a5db6f0085342e17ed4f98fd2f5078
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-29 16:30:15 +0200

    init and title on index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Badwords</title>
+</head>
+
+<body>
+    <div class="container bg">
+        <h1 class="bg-danger text-center text-light ">Bad words</h1>
+    </div>
+</body>
+
+</html>
